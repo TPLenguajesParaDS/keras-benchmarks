bedd3c77010917da0d1f8ce108b42ef6ec7fb1ac
jmarshall94 <145154047+jmarshall94@users.noreply.github.com>
2024-05-02 15:18:59 -0300

    Update build.sh

--- shell/build.sh
+++ shell/build.sh
@@ -6,31 +6,31 @@ image_already_exists() {
 }
 
 # Build Base cuda with python 3.10 image
-if ! image_exists "cuda-python310:latest"; then
+if ! image_already_exists "cuda-python310:latest"; then
     docker build -t cuda-python310 -f Dockerfile .
 fi
 
 # Build Tensorflow standalone image
-if ! image_exists "tensorflow"; then
+if ! image_already_exists "tensorflow"; then
     docker build -t tensorflow -f Dockerfile.tensorflow .
 fi
 
 # Build Torch standalone image
-if ! image_exists "torch"; then
+if ! image_already_exists "torch"; then
     docker build -t torch -f Dockerfile.torch .
 fi
 
 # Build Keras with torch backend image
-if ! image_exists "keras-torch"; then
+if ! image_already_exists "keras-torch"; then
     docker build -t keras-torch -f Dockerfile.keras-torch .
 fi
 
 # Build Keras with jax backend image
-if ! image_exists "keras-jax"; then
+if ! image_already_exists "keras-jax"; then
     docker build -t keras-jax -f Dockerfile.keras-jax .
 fi
 
 # Build Keras with tensorflow backend image
-if ! image_exists "keras-tensorflow"; then
+if ! image_already_exists "keras-tensorflow"; then
     docker build -t keras-tensorflow -f Dockerfile.keras-tensorflow .
 fi
